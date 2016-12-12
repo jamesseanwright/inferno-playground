8261914ba844b55d27ac71654e77af73366a9ef1
James Wright <james.wright@net-a-porter.com>
2016-12-12 17:11:42 +0000

    iterator key

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -6,7 +6,7 @@ const Character = require('./Character');
 module.exports = function App({ codes }) {
     return (
         <ul>
-            {codes.map(c => <Character code={c} />)}
+            {codes.map(c => <Character key={c} code={c} />)}
         </ul>
     );
 };
